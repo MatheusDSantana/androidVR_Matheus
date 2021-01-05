b55c40416d6ced4f3ec3f6580b6e78b7aca5b3be
MatheusDSantana <matheusdsantana07@gmail.com>
2021-01-05 16:31:31 -0300

    apk file

---
+++ Assets/endOfLvl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class endOfLvl : MonoBehaviour
+{
+    public void goToLvlOne()
+    {
+        //Debug.Log("HIT");
+        SceneManager.LoadScene("First Person Shooter");
+    }
+
+    public void endApp()
+    {
+        //Debug.Log("HIT");
+        Application.Quit();
+    }
+}
